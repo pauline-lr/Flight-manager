60caa0153ed098024daf674a04bd531a0f657d65
pauline-lr <etu40989@henallux.be>
2021-05-14 15:20:56 +0200

    suppressions de variables inutiles

--- Projet/src/view/panels/menuBarPanels/optionsFlightPanels/AddFlightPanel.java
+++ Projet/src/view/panels/menuBarPanels/optionsFlightPanels/AddFlightPanel.java
@@ -1,22 +1,14 @@
 package view.panels.menuBarPanels.optionsFlightPanels;
 
-
-import controller.ApplicationController;
-import exception.FlightException;
-import model.Flight;
 import view.forms.flightForms.FlightForm;
 import view.panels.buttons.ButtonsPanel;
 import view.windows.MenuWindow;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class AddFlightPanel extends JPanel {
-    private ApplicationController controller;
     private MenuWindow menuWindow;
-    private JButton retour, validation, réinit;
     private FlightForm flightForm;
 
     public AddFlightPanel(MenuWindow menuWindow) {
@@ -26,40 +18,4 @@ public class AddFlightPanel extends JPanel {
         this.add(flightForm, BorderLayout.CENTER);
         this.add(new ButtonsPanel(menuWindow, "Addition", flightForm), BorderLayout.SOUTH);
     }
-
-
-    // bouton de validation
-    /*private class ValidationListener(Flight flightForm) implements ActionListener{
-        @Override
-        public void actionPerformed(ActionEvent evt){
-          Flight flight = flightForm.getFlight();
-            try {
-                flight = flightForm.getFlight();
-                controller.addFlight(flight);
-                JOptionPane.showMessageDialog(null, "Vol ajouté", "Succès", JOptionPane.INFORMATION_MESSAGE);
-                takeOut();
-            } catch (FlightException.NumberFlightException e) {
-                e.printStackTrace();
-            } catch (FlightException.MealDescriptionException e) {
-                e.printStackTrace();
-            }
-        }
-    }*/
-
-    // bouton de réinitialisation
-   /* private class RéinitListener implements ActionListener{
-        @Override
-        public void actionPerformed(ActionEvent evt){
-            takeOut();
-        }
-    }
-
-    public void takeOut(){
-        menuWindow.getCont().removeAll();
-        menuWindow.getCont().add(new AddFlightPanel(menuWindow),BorderLayout.CENTER);
-        menuWindow.getCont().repaint();
-        menuWindow.setVisible(true);
-    }*/
-
-
 }
--- Projet/src/view/panels/menuBarPanels/optionsFlightPanels/ModifyFlightPanel.java
+++ Projet/src/view/panels/menuBarPanels/optionsFlightPanels/ModifyFlightPanel.java
@@ -1,9 +1,6 @@
 package view.panels.menuBarPanels.optionsFlightPanels;
 
-import controller.ApplicationController;
-import exception.DataBaseConnectionException;
-import exception.FlightException;
-import model.Flight;
+
 import view.forms.flightForms.FlightForm;
 import view.forms.flightForms.ModifyFlightForm;
 import view.panels.buttons.ButtonsPanel;
@@ -11,14 +8,10 @@ import view.windows.MenuWindow;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.SQLException;
+
 
 public class ModifyFlightPanel extends JPanel {
-    private ApplicationController controller;
     private MenuWindow menuWindow;
-    private ModifyFlightForm modifyFlightForm;
     private FlightForm flightForm;
 
     public ModifyFlightPanel(MenuWindow menuWindow) {
@@ -29,47 +22,5 @@ public class ModifyFlightPanel extends JPanel {
         this.add(flightForm, BorderLayout.LINE_START);
         this.add(new ButtonsPanel(menuWindow, "Modification", flightForm), BorderLayout.SOUTH);
     }
-
-    // bouton de validation
-  /*private class ValidationListener(Flight flightForm) implements ActionListener{
-        @Override
-        public void actionPerformed(ActionEvent evt){
-            Flight flight = flightForm.getFlight();
-            try {
-                flight = flightForm.getFlight();
-               // if(numéro de flight a été modifé){
-                    controller.modifyFlight(flight, flight.getNumber());
-                }else{
-                    controller.modifyFlight(flight);
-                }
-                JOptionPane.showMessageDialog(null, "Vol modifié", "Succès", JOptionPane.INFORMATION_MESSAGE);
-                takeOut();
-            } catch (FlightException.NumberFlightException e) {
-                e.printStackTrace();
-            } catch (FlightException.MealDescriptionException e) {
-                e.printStackTrace();
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            } catch (DataBaseConnectionException e) {
-                e.printStackTrace();
-            }
-        }
-    }*/
-
-    // bouton de réinitialisation
-  /* private class RéinitListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent evt){
-            takeOut();
-        }
-    }
-
-    public void takeOut(){
-        menuWindow.getCont().removeAll();
-        menuWindow.getCont().add(new ModifyFlightPanel(menuWindow),BorderLayout.CENTER);
-        menuWindow.getCont().repaint();
-        menuWindow.setVisible(true);
-    }*/
-
 }
 
