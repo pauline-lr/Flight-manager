6f41458ae45f32820684928e5d6201ff1b9ec2fc
pauline-lr <etu40989@henallux.be>
2021-05-21 15:53:11 +0200

    jtable

--- Projet/src/view/resultSearch/ResultSearchFlightsByPilot.java
+++ Projet/src/view/resultSearch/ResultSearchFlightsByPilot.java
@@ -1,5 +1,18 @@
 package view.resultSearch;
 
-public class ResultSearchFlightsByPilot {
+import model.SearchFlightsByPilot;
+import view.tables.SearchFlightsByPilotTable;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class ResultSearchFlightsByPilot extends JPanel {
+    public ResultSearchFlightsByPilot(ArrayList<SearchFlightsByPilot> flights) {
+        SearchFlightsByPilotTable flightsTable = new SearchFlightsByPilotTable(flights);
+        this.setLayout(new BorderLayout());
+        JTable table = new JTable(flightsTable);
+        table.setModel(flightsTable);
+        this.add(new JScrollPane(table), BorderLayout.CENTER);
+    }
 }
