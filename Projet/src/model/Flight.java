b75b8ff004a2b2f851cd4e4e124864561fd3e10a
pauline-lr <etu40989@henallux.be>
2021-05-23 12:09:18 +0200

    up

--- Projet/src/model/Flight.java
+++ Projet/src/model/Flight.java
@@ -49,15 +49,17 @@ public class Flight {
     private void setNumber(String number) throws FlightException.NumberFlightException {
             Pattern pattern = Pattern.compile(REGEX_NUMBER);
             Matcher matcher = pattern.matcher(number);
-            if (matcher.find()) {
+            // grâce au code regex, pas de test supplémentaire pour le cas où vol est vide
+            if (matcher.find() /*&& ne se trouve pas déjà dans la BD*/) {
                 this.number = number;
             } else {
                 throw new FlightException.NumberFlightException(number);
             }
     }
+
     private void setDepartureTime(GregorianCalendar departureTime) {
         GregorianCalendar currentDate = new GregorianCalendar();
-        if (departureTime.compareTo(currentDate) > 0) {
+        if (departureTime.compareTo(currentDate) >= 0) {
             this.departureTime = departureTime;
         } else {
             try {
@@ -125,7 +127,6 @@ public class Flight {
         return mealDescription;
     }
     public String getPilot() {
-        // on obtient la clé (en string) pour rechercher pilot dans la BD
         return pilot;
     }
     public String getDepartureGate() {
